8f46fe5c715ef36504f79861fe1a873a95c7951e
Aaron Harris <aaronsharris@gmail.com>
2021-12-08 16:54:18 -0600

    added node-sass, added component and pages folders, added home page, menu item, directory compoents and scss files

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Home.scss'
+import Directory from '../../components/directory/Directory'
+export const Home = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
